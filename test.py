7596f152e95814751d771c84480af5e795911a11
Sorin Marian <sorinnicumarian@gmail.com>
2024-11-26 03:14:39 +0200

    first working test

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ from datetime import datetime, timedelta
 import json
 from tikapi import TikAPI, ValidationException, ResponseException
 
-
 def load_config(config_file="config.json"):
     with open(config_file, 'r') as f:
         return json.load(f)
@@ -12,21 +11,58 @@ def load_config(config_file="config.json"):
 config = load_config()  # Load config data
 api = tikapi.TikAPI(config["tikapi_key"])
 
-def get_videos_by_hashtag(hashtag_name):
-
+def get_videos_by_hashtag(hashtag_name, max_videos=2):
+    videos = []
     response = api.public.hashtag(name=hashtag_name)
-    hashtag_id = response.json()['challengeInfo']['challenge']['id']
-    response = api.public.hashtag(id=hashtag_id)
+    
+    if 'challengeInfo' in response.json() and 'challenge' in response.json()['challengeInfo']:
+        hashtag_id = response.json()['challengeInfo']['challenge']['id']
+        response = api.public.hashtag(id=hashtag_id)
+        
+        while response:
+            items_list = response.json().get('itemList', [])
+            video_ids = []
 
-    while(response):
-        cursor = response.json().get('cursor')
-        response = response.next_items()
+            for item in items_list:
+                video_id = item.get('id', [])
+                video_ids.append(video_id)
+    return videos
 
 def get_video_info_by_id(video_id):
     video_response = api.public.video(id=video_id)
-    #TODO write code to get the following info: video number of views, likes, comments, creation date, author_username, author_verified, author_followers, author_following, author_likes_count, author_videos_count, author_creation_date
-    return 
+    video_data = video_response.json()
+    
+    # Extract the requested video info
+    video_info = {
+        "video_id": video_id,
+        "views": video_data.get('stats', {}).get('playCount'),
+        "likes": video_data.get('stats', {}).get('diggCount'),
+        "comments": video_data.get('stats', {}).get('commentCount'),
+        "creation_date": video_data.get('createTime'),
+        "author_username": video_data.get('author', {}).get('uniqueId'),
+        "author_verified": video_data.get('author', {}).get('verified'),
+        "author_followers": video_data.get('author', {}).get('followerCount'),
+        "author_following": video_data.get('author', {}).get('followingCount'),
+        "author_likes_count": video_data.get('author', {}).get('heartCount'),
+        "author_videos_count": video_data.get('author', {}).get('videoCount'),
+        "author_creation_date": video_data.get('author', {}).get('createTime')
+    }
+    
+    return video_info
 
-#TODO write code for a main function
-#TODO get only the first 2 videos
-get_videos_by_hashtag("echilibrusiverticalitate")
+def main():
+    hashtag_name = "echilibrusiverticalitate"
+    max_videos = 2  # Get only the first 2 videos
+
+    # Get video IDs by hashtag
+    video_ids = get_videos_by_hashtag(hashtag_name, max_videos)
+    if video_ids:
+        # Fetch and print info for each video
+        for video_id in video_ids:
+            video_info = get_video_info_by_id(video_id)
+            print(json.dumps(video_info, indent=4))
+    else:
+        print(f"No videos found for hashtag '{hashtag_name}'.")
+
+if __name__ == "__main__":
+    main()
